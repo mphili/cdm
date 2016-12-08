3dd7b95f30809087b6bf9e4c67c28ab1e353637a
Michel Philipp <michel.philipp.mp@gmail.com>
2016-12-08 09:26:22 +0100

    Changes in calc_lxa.

--- src/calc_lxa.cpp
+++ src/calc_lxa.cpp
@@ -1,52 +1,29 @@
-#include <RcppArmadillo.h>
 #include <Rcpp.h>
-#include <algorithm>
 
-// [[Rcpp::depends(RcppArmadillo)]]
-
-using namespace arma;
 using namespace Rcpp;
 
-// [[Rcpp::export]]
-RcppExport SEXP calc_lxa(SEXP Xs, SEXP Ps) {
+RcppExport SEXP calc_lxa(SEXP data, SEXP Palpha) {
 BEGIN_RCPP
 
-  NumericMatrix Xr(Xs);
-  NumericMatrix Pr(Ps);
+  NumericMatrix x(data);
+  NumericMatrix pa(Palpha);
 
-  int n = Xr.nrow(), j = Xr.ncol(), l = Pr.ncol();
-  
-  mat x(Xr.begin(), n, j, false);
-  mat pa(Pr.begin(), j, l, false);
-  
-  mat post = exp(x * log(pa) + (1-x) * log(1-pa));
-  return wrap(post);
-  
+  unsigned int I = x.nrow();
+  unsigned int J = x.ncol();
+  unsigned int L = pa.ncol();
+
+  NumericMatrix post(I, L);
+  std::fill(post.begin(), post.end(), 1.0);
+     
+  for (int i = 0; i < I; i++) {
+    for (int l = 0; l < L; l++) {
+      for (int j = 0; j < J; j++) {
+        post(i,l) *= ((x(i,j) == 1) ? pa(j,l) : (1-pa(j,l)));
+      }
+    }
+  }
+     
+  return post;
+
 END_RCPP
 }
-
-// RcppExport SEXP calc_lxa2(SEXP data, SEXP Palpha) {
-// BEGIN_RCPP
-// 
-//   NumericMatrix x(data);
-//   NumericMatrix pa(Palpha);
-// 
-//   unsigned int I = x.nrow();
-//   unsigned int J = x.ncol();
-//   unsigned int L = pa.ncol();
-// 
-//   NumericMatrix post(I, L);
-//   std::fill(post.begin(), post.end(), 1.0);
-//      
-//   for (int i = 0; i < I; i++) {
-//     for (int l = 0; l < L; l++) {
-//       for (int j = 0; j < J; j++) {
-//         post(i,l) *= ((x(i,j) == 1) ? pa(j,l) : (1-pa(j,l)));
-//       }
-//     }
-//   }
-//      
-//   return post;
-// 
-// END_RCPP
-// }
